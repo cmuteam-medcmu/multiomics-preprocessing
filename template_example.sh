3e1f357f4a80dd34510b67999c6b5a1b8d8dd7d4
Sutthittha <119776865+Moonipur@users.noreply.github.com>
2026-01-25 23:12:43 +0700

    Add unit comment for MEMORY variable
    Added unit comment for MEMORY variable.

--- template_example.sh
+++ template_example.sh
@@ -12,7 +12,7 @@ set -euo pipefail
 # 1. VARIABLE SETTING
 # Use readonly for variables that should not change
 readonly THREADS=8
-readonly MEMORY=20
+readonly MEMORY=20 # Gb unit
 readonly IN_DIR="./data/raw"
 readonly OUT_DIR="./data/processed"
 readonly REPORT_DIR="./reports"
